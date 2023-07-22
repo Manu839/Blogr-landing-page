283081eb9fea63e8045e297445047b51e9f57133
Manu839 <124304448+Manu839@users.noreply.github.com>
2023-07-23 01:33:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import SectionOne from "./components/SectionOne"
+import SectionTwo from "./components/SectionTwo"
+import SectionThree from "./components/SectionThree"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main>
+        <Hero/>
+        <SectionOne/>
+        <SectionTwo/>
+        <SectionThree/>
+        <Footer/>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+
+      colors: {
+        // primary
+        'light-red': 'hsl(356, 100%, 66%)',
+        'very-light-red': 'hsl(355, 100%, 74%)',
+        'very-dark-blue': 'hsl(208, 49%, 24%)',
+        // neutral
+        'white': 'hsl(0, 0%, 100%)',
+        'light-gray': '#EFEFF1',
+        'grayish-blue': 'hsl(240, 2%, 79%)',
+        'very-dark-grayish-blue': 'hsl(207, 13%, 34%)',
+        'very-dark-black-blue': 'hsl(240, 10%, 16%)',
+        // gradient
+        'very-light-red': 'hsl(13, 100%, 72%)',
+        'light-red': 'hsl(353, 100%, 62%)',
+        'very-dark-gray-blue': 'hsl(237, 17%, 21%)',
+        'very-dark-gray-desaturated-blue': 'hsl(237, 23%, 32%)',
+        'cta-hover': '#FF7B86',
+      },
+      fontFamily: {
+        'heading': ['"Overpass"', 'sans-serif'],
+        'body': ['"Ubuntu"', 'sans-serif'],
+    },
+    backgroundPosition: {
+      'right-top-4': 'right -80rem top -83rem',
+      'right-top-3': 'right -33rem top -15rem',
+    },
+  },
+  plugins: [],
+}
+}
